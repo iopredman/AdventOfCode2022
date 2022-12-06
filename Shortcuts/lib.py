84124de4cfd538a657dbf1ee90b5f43d2333424f
iopredman <iopredman@gmail.com>
2022-12-06 17:00:07 -0500

    Add files via upload

---
+++ Shortcuts/lib.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression 
+
---
+++ Shortcuts/sc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+p = print
+l = len
+r = range
+e = enumerate
+m = map
+i = int
+s = str
+li = list
+
